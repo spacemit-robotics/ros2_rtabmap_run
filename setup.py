6d6e1198959ca3791c1fcef94b428f3729ed3532
chenzhaoqi <zhaoqi.chen@spacemit.com>
2026-04-28 20:00:43 +0800

    feat(rtabmap): add ROS2 RTABMAP SLAM launch package
    Add rtabmap_run package to support RGB-D and 3D LiDAR SLAM capabilities.
    
    Changes:
    - Add package.xml with ROS2 dependencies
    - Add rgbd_slam.launch.py for RGB-D camera SLAM
    - Add 3d_lidar_slam.launch.py for 3D LiDAR SLAM
    - Add README.md and README_en.md with usage documentation
    - Add Python package structure (setup.py, setup.cfg)

---
+++ setup.py
@@ -0,0 +1,35 @@
+# Copyright 2026 SpacemiT (Hangzhou) Technology Co. Ltd.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from setuptools import find_packages, setup
+import os
+import glob
+package_name = 'rtabmap_run'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob.glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot_team',
+    maintainer_email='robot_team@spacemit.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
